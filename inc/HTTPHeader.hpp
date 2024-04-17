9c82b9755e3f2b8449116697cdd3328691e6e7d0
Nacho Mota 🚀 <nacho.mota@enunpimpam.com>
2024-04-17 12:31:41 +0200

    HTTPHeader en cpp

--- inc/HTTPHeader.hpp
+++ inc/HTTPHeader.hpp
@@ -6,7 +6,7 @@
 /*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 17:32:08 by nmota-bu          #+#    #+#             */
-/*   Updated: 2024/04/17 12:23:45 by nmota-bu         ###   ########.fr       */
+/*   Updated: 2024/04/17 12:30:33 by nmota-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,92 +17,6 @@
 #include <string>
 #include <map>
 
-// class HTTPHeader
-// {
-// private:
-//     typedef std::map<std::string, std::string> FieldMap;
-//     FieldMap fields;
-
-// public:
-//     HTTPHeader(const std::string &clientResponse)
-//     {
-//     }
-
-//     // Método para agregar un campo al encabezado HTTP
-//     void addField(const std::string &key, const std::string &value)
-//     {
-//         fields[key] = value;
-//     }
-
-//     // Método para obtener el encabezado HTTP como una cadena
-//     std::string getHeader() const
-//     {
-//         std::string header;
-//         for (FieldMap::const_iterator it = fields.begin(); it != fields.end(); ++it)
-//         {
-//             header += it->first + ": " + it->second + "\r\n";
-//         }
-//         header += "\r\n";
-//         return header;
-//     }
-
-// };
-
-//========================PARSEANDO BUFFER==========================================
-
-// class HTTPHeader
-// {
-// private:
-//     typedef std::map<std::string, std::string> FieldMap;
-//     FieldMap fields;
-
-// public:
-//     // Constructor que toma la respuesta del cliente como argumento
-//     HTTPHeader(const std::string &clientResponse)
-//     {
-//         parseClientResponse(clientResponse);
-//     }
-
-//     // Método para agregar un campo al encabezado HTTP
-//     void addField(const std::string &key, const std::string &value)
-//     {
-//         fields[key] = value;
-//     }
-
-//     // Método para obtener el encabezado HTTP como una cadena
-//     std::string getHeader() const
-//     {
-//         std::string header;
-//         for (FieldMap::const_iterator it = fields.begin(); it != fields.end(); ++it)
-//         {
-//             header += it->first + ": " + it->second + "\r\n";
-//         }
-//         header += "\r\n";
-//         return header;
-//     }
-
-// private:
-//     // Método privado para analizar la respuesta del cliente y extraer los campos relevantes
-//     void parseClientResponse(const std::string &clientResponse)
-//     {
-//         std::string::size_type pos = 0;
-//         std::string::size_type prevPos = 0;
-//         while ((pos = clientResponse.find("\r\n", prevPos)) != std::string::npos)
-//         {
-//             std::string line = clientResponse.substr(prevPos, pos - prevPos);
-//             prevPos = pos + 2; // Avanzar la posición inicial para la próxima iteración
-//             std::string::size_type colonPos = line.find(": ");
-//             if (colonPos != std::string::npos)
-//             {
-//                 std::string key = line.substr(0, colonPos);
-//                 std::string value = line.substr(colonPos + 2);
-//                 addField(key, value);
-//             }
-//         }
-//     }
-// };
-//=========================================================================
-
 class HTTPHeader
 {
 private:
@@ -111,74 +25,18 @@ private:
     std::string _header;
 
 public:
-    HTTPHeader(const std::string &prot, const std::string &code)
-    {
-        std::string tmp = prot + " " + code + "\r\n";
-        _header = tmp;
-        addField("Date", DateField());
-    }
+    HTTPHeader(const std::string &prot, const std::string &code);
+    ~HTTPHeader();
 
-    std::string DateField() const
-    {
-        // Obtener la fecha y hora actual
-        time_t currentTime;
-        time(&currentTime);
-        struct tm *timeInfo = gmtime(&currentTime);
-
-        // Formatear la fecha según el estándar RFC 1123 (ejemplo: "Tue, 15 Nov 1994 08:12:31 GMT")
-        char buffer[80];
-        strftime(buffer, sizeof(buffer), "%a, %d %b %Y %H:%M:%S GMT", timeInfo);
-        return buffer;
-    }
+    std::string DateField() const;
 
     // Método para agregar un campo al encabezado HTTP
-    void addField(const std::string &key, const std::string &value)
-    {
-        _fields[key] = value;
-    }
+    void addField(const std::string &key, const std::string &value);
 
     // Método para obtener el encabezado HTTP como una cadena
-    std::string getHeader() const
-    {
-        std::string header = _header;
+    std::string getHeader() const;
 
-        for (FieldMap::const_iterator it = _fields.begin(); it != _fields.end(); ++it)
-            header += it->first + ": " + it->second + "\r\n";
-        header += "\r\n";
-        return header;
-    }
-
-    size_t headerLength() const { return _header.length(); }
+    size_t headerLength() const;
 };
-//=========================================================================
-// class HTTPHeader
-// {
-// private:
-//     std::string _header;
-
-// public:
-//     HTTPHeader() {}
-//     ~HTTPHeader() {}
-
-//     void addField(const std::string &key, const std::string &value)
-//     {
-//         _header += key + ": ";
-//         _header += value + "\r\n";
-//     }
-
-//     void addField(const std::string &simple)
-//     {
-//         _header += simple + "\r\n\r\n";
-//     }
-
-//     const std::string &getHeader() const
-//     {
-
-//         // _header += "\r\n";
-//         return _header;
-//     }
-
-//     size_t headerLength() const { return _header.length(); }
-// };
 
 #endif
--- src/HTTPHeader.cpp
+++ src/HTTPHeader.cpp
@@ -6,8 +6,47 @@
 /*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 11:24:48 by nmota-bu          #+#    #+#             */
-/*   Updated: 2024/04/17 11:46:20 by nmota-bu         ###   ########.fr       */
+/*   Updated: 2024/04/17 12:30:34 by nmota-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HTTPHeader.hpp"
+
+HTTPHeader::HTTPHeader(const std::string &prot, const std::string &code)
+{
+	std::string tmp = prot + " " + code + "\r\n";
+	_header = tmp;
+	addField("Date", DateField());
+}
+
+HTTPHeader::~HTTPHeader() {}
+
+std::string HTTPHeader::DateField() const
+{
+	// Obtener la fecha y hora actual
+	time_t currentTime;
+	time(&currentTime);
+	struct tm *timeInfo = gmtime(&currentTime);
+
+	// Formatear la fecha según el estándar RFC 1123 (ejemplo: "Tue, 15 Nov 1994 08:12:31 GMT")
+	char buffer[80];
+	strftime(buffer, sizeof(buffer), "%a, %d %b %Y %H:%M:%S GMT", timeInfo);
+	return buffer;
+}
+
+void HTTPHeader::addField(const std::string &key, const std::string &value)
+{
+	_fields[key] = value;
+}
+
+std::string HTTPHeader::getHeader() const
+{
+	std::string header = _header;
+
+	for (FieldMap::const_iterator it = _fields.begin(); it != _fields.end(); ++it)
+		header += it->first + ": " + it->second + "\r\n";
+	header += "\r\n";
+	return header;
+}
+
+size_t HTTPHeader::headerLength() const { return _header.length(); }
