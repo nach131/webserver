bb1b0708f468c7cf5b925daa77b6fad44c03bd8c
Valerio Duchi <vduchi@car9s6.42barcelona.com>
2024-04-28 16:05:03 +0200

    Started configuration class

--- inc/ConfigServer.hpp
+++ inc/ConfigServer.hpp
@@ -6,18 +6,41 @@
 /*   By: vduchi <vduchi@student.42barcelona.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 16:10:22 by vduchi            #+#    #+#             */
-/*   Updated: 2024/03/12 16:11:06 by vduchi           ###   ########.fr       */
+/*   Updated: 2024/04/28 15:23:07 by vduchi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CONFIGSERVER_H__
 # define CONFIGSERVER_H__
 
-# include "WebServer.hpp"
+# include <map>
+# include <vector>
+# include <fstream>
+# include <sstream>
+# include <iostream>
+# include "Colors.hpp"
+
+class Location
+{
+	private:
+		Location();
+		std::map<std::string, std::string> _val;
+
+	public:
+		Location(const std::string &);
+		~Location();
+};
 
 class ConfigServer
 {
 	private:
+		ConfigServer();
+		std::map<std::string, std::string> _gen;
+		std::multimap<std::string, Location *> _loc;
+
+	public:
+		ConfigServer(const std::string &);
+		~ConfigServer();
 };
 
 #endif
--- inc/WebServer.hpp
+++ inc/WebServer.hpp
@@ -6,19 +6,19 @@
 /*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 15:34:48 by vduchi            #+#    #+#             */
-/*   Updated: 2024/04/25 21:26:26 by nmota-bu         ###   ########.fr       */
+/*   Updated: 2024/04/28 13:36:07 by vduchi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WEBSERVER_H__
 # define WEBSERVER_H__
 
-#include "Colors.hpp"
+# include "Colors.hpp"
 # include <map>
 # include <fstream>
 # include <iostream>
-#include <sstream>
-#include <functional>
+# include <sstream>
+# include <functional>
 
 int start();
 
