3aaf307a8fc0c3f164671f8c65c1dedee9a5c735
Nacho Mota 🚀 <nacho.mota@enunpimpam.com>
2024-05-29 00:05:18 +0200

    otro upload

---
+++ cgi_bin/tmp/upload.py
@@ -0,0 +1,23 @@
+import sys
+
+def write_to_file(filename, data):
+    try:
+        with open(filename, 'wb') as file:
+            file.write(data)
+            print(f"File saved: {filename}")
+            return 0
+    except IOError:
+        print("Error: Unable to open file for writing.")
+        return 1
+
+if __name__ == "__main__":
+    filname = sys.argv[1]
+    data = sys.argv[2].encode()  # Convert data to bytes
+    result = write_to_file(filname, data)
+    print(result)
+
+
+# Datos binarios de ejemplo
+# data = 'datos binarios.'
+# Nombre del archivo
+# filename = 'example.bin'
